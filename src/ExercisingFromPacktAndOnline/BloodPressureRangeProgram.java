0b07eb2c8b777f2b70beb55efd2be0fc4d17f3c3
Ozioma <oziomaokoroafor@gmail.com>
2021-09-12 09:50:18 +0100

    Blood pressure program

---
+++ src/ExercisingFromPacktAndOnline/BloodPressureRangeProgram.java
@@ -0,0 +1,25 @@
+package ExercisingFromPacktAndOnline;
+
+import java.util.Scanner;
+
+public class BloodPressureRangeProgram {
+    public static int input(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+    public static void evaluateSystolic(){
+        int input = input();
+        boolean isSystolic = 90 <= input;
+        boolean isAboveSystolic = input > 120;
+        if ( isSystolic ){
+            System.out.println("Perfect!");
+        }
+        else if ( isAboveSystolic) System.out.println("Above");
+        else {
+            System.out.println("Below");}
+    }
+
+    public static void main(String[] args) {
+        evaluateSystolic();
+    }
+}
