e9268f76febce69e0bfc384536ac90e3f60f538d
Ozioma <oziomaokoroafor@gmail.com>
2021-09-14 20:34:43 +0100

    Arrays- copy of range practice refactored commit

--- src/ExercisingFromPacktAndOnline/ArraysCopyOfRange.java
+++ src/ExercisingFromPacktAndOnline/ArraysCopyOfRange.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class ArraysCopyOfRange {
     public static void main(String[] args) {
-        int[] numbers = new int[5];
+        int [] numbers = new int[5];
         Arrays.fill(numbers, 1);
         System.out.println(Arrays.toString(numbers));
-        int [] shortNumbers = Arrays.copyOfRange(numbers, 0, 2);
+        int [] shortNumbers = Arrays.copyOfRange(numbers, 0, 3);
         System.out.println(Arrays.toString(shortNumbers));
-        int [] longNumbers = Arrays.copyOf(numbers, 10);
+        int [] longNumbers = Arrays.copyOf(numbers, 6);
         System.out.println(Arrays.toString(longNumbers));
     }
-}
+    }
