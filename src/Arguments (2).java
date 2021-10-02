41d2e0e1b583ac4b755d081e999ceb8e4c79c4d9
Ozioma <oziomaokoroafor@gmail.com>
2021-10-02 01:44:58 +0100

    Deleting files

---
+++ src/Arguments (2).java
@@ -0,0 +1,18 @@
+/* The function takes two arrays
+one is an array, the other is an integer
+arg[0] is an array of integers
+arg[1] is an integer
+
+Write a function that takes two argument (arg[0] and arg[1]),
+such that when the function is called, returns 2 indices of numbers in arg[0]
+that equals arg[1]
+sample arg[0] = {1,2,3,4,5}, arg[1] = 3
+1+2 = 3, so the index of 1, is 0 and the index of 2, is 1
+function returns {0,1}.
+*/
+
+public class Arguments {
+    public static void main(String[] args) {
+        int b;
+        }
+    }
