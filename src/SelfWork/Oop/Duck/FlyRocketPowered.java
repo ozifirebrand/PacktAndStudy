f5a3adf2f7be19e73b82613b8cc361c33c3ce16e
Ozioma <oziomaokoroafor@gmail.com>
2022-01-26 08:32:02 +0100

    Rocket powered flying implementation

---
+++ src/SelfWork/Oop/Duck/FlyRocketPowered.java
@@ -0,0 +1,8 @@
+package SelfWork.Oop.Duck;
+
+public class FlyRocketPowered implements FlyBehaviour{
+    @Override
+    public void fly() {
+        System.out.println("I am flying with a rocket!");
+    }
+}
