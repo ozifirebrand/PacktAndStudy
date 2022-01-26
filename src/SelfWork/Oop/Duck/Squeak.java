f32d035d376f5180898d88327ccc07dd389630cf
Ozioma <oziomaokoroafor@gmail.com>
2022-01-26 05:51:04 +0100

    Quackable squeak implementation

---
+++ src/SelfWork/Oop/Duck/Squeak.java
@@ -0,0 +1,8 @@
+package SelfWork.Oop.Duck;
+
+public class Squeak implements QuackBehaviour{
+    @Override
+    public void quack() {
+        System.out.println("Scream like a Chicken");
+    }
+}
