9d470a3263649715082e6f46f887e05a0adbf34d
Ozioma <oziomaokoroafor@gmail.com>
2022-01-26 06:00:11 +0100

    Methods implementation

--- src/SelfWork/Oop/Duck/MallardDuck.java
+++ src/SelfWork/Oop/Duck/MallardDuck.java
@@ -3,7 +3,11 @@ package SelfWork.Oop.Duck;
 public class MallardDuck extends Duck{
 
     public MallardDuck(){
-//        quackBehaviour= new Quack();
-        flyBehaviour =
+        quackBehaviour= new Quack();
+        flyBehaviour = new FlyWithWings();
+    }
+
+    public void display(){
+        System.out.println("I am a real Mallard Duck");
     }
 }
