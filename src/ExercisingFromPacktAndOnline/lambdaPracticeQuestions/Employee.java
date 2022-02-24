5d67d5244fc3b839e83e57a15ea371e6fa43e2ca
Ozioma <oziomaokoroafor@gmail.com>
2022-02-24 11:31:34 +0100

    Employee commit

---
+++ src/ExercisingFromPacktAndOnline/lambdaPracticeQuestions/Employee.java
@@ -0,0 +1,8 @@
+package ExercisingFromPacktAndOnline.lambdaPracticeQuestions;
+
+public class Employee {
+
+    private void calculateEmployeesSalary(){
+//        Measurable measurable = (int x )->System.out.println(2 * x);
+    }
+}
